7126c6f442a3b70f3ee7888b9d1a3f1f92bc31b0
Ade Novid <adenvt@gmail.com>
2018-06-28 21:58:17 +0700

    add datepicker

--- resources/assets/js/coreui/main.js
+++ resources/assets/js/coreui/main.js
@@ -2,11 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import Datepicker from 'vuejs-datepicker'
+import { id } from 'vuejs-datepicker/dist/locale'
 import App from './App'
 import router from './router'
 import store from './store'
 
 Vue.use(BootstrapVue)
+
+Vue.component('b-datepicker', {
+  extends: Datepicker,
+  props  : {
+    bootstrapStyling: {
+      type   : Boolean,
+      default: true,
+    },
+    language: {
+      type   : Object,
+      default: () => id,
+    },
+  },
+})
 
 window.Vue = new Vue({
   el        : '#app',
